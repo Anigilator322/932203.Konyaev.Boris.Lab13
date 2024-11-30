07e645059dd863e6a059e6a3a014c04c2a385100
Boris <66841815+Anigilator322@users.noreply.github.com>
2024-11-30 15:29:29 +0700

    Init

---
+++ Lab13/Controllers/MockupsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab13.Controllers
+{
+    public class MockupsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("index");
+        }
+    }
+}
